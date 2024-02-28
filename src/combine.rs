a1c7299647d561f42dbddd0d2a54a2f264c1ecd4
Jonas Kernchen <kernchen.jonas@gmail.com>
2024-02-28 23:10:32 +0100

    mul test

--- src/combine.rs
+++ src/combine.rs
@@ -26,38 +26,39 @@ fn select_helper<T: Clone>(refs: &Vec<T>, n: usize) -> Vec<Vec<T>> {
     return result;
 }
 
-pub fn multiply<T>(a: &Vec<&Vec<T>>) -> Vec<Vec<T>> {
+pub fn multiply<T: Copy>(a: &Vec<&Vec<T>>) -> Vec<Vec<T>> {
     let mut max_iter = 100;
     let mut result: Vec<Vec<T>> = vec![];
     let mut counts: Vec<[usize; 2]> = vec![];
     for ae in a {
         counts.push([ae.len(), ae.len()]);
     }
+    let len = counts.len();
     'a: while max_iter > 0 {
         max_iter -= 1;
-        let n: Vec<T> = vec![];
-        println!("{:?}", counts);
-        
+        let mut n: Vec<T> = vec![];
+
         // TODO add combination to n
-        for c in counts.iter().enumerate() {}
+        for i in 0..len {
+            n.push(a[i][counts[i][0] - 1]);
+        }
 
         // add n to result
         result.push(n);
-        
+
         // decrease count
         // TODO add last count to loop
-        let len = counts.len();
         for i in 0..len {
             use std::cmp::Ordering;
             let c = &mut counts[i];
             match c[0].cmp(&1) {
                 Ordering::Greater => {
                     c[0] -= 1;
-                    if counts.iter().all(|count| {count[0] == 1}) {
+                    if counts.iter().all(|count| count[0] == 1) {
                         break 'a;
                     }
                     break;
-                },
+                }
                 _ => c[0] = c[1],
             }
         }
@@ -76,21 +77,26 @@ mod combine_tests {
 
         let c = vec![&a, &b];
 
-        let t = multiply(&c);
+        let my_result = multiply(&c);
+        let correct_result = vec![
+            vec![1, 4],
+            vec![1, 5],
+            vec![1, 6],
+            vec![2, 4],
+            vec![2, 5],
+            vec![2, 6],
+            vec![3, 4],
+            vec![3, 5],
+            vec![3, 6],
+        ];
 
         assert_eq!(
-            t,
-            vec![
-                vec![1, 4],
-                vec![1, 5],
-                vec![1, 6],
-                vec![2, 4],
-                vec![2, 5],
-                vec![2, 6],
-                vec![3, 4],
-                vec![3, 5],
-                vec![3, 6],
-            ]
+            my_result.len(),
+            correct_result.len(),
+            "comparing your vec:\n{:?}\nwith the correct vec:\n{:?}",
+            my_result,
+            correct_result
         );
+        assert!(my_result.iter().all(|e| { correct_result.contains(e) }));
     }
 }
